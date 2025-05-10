6007416072a93af398a961a645532764792f6e65
Runar Nazmutdinov <runarnazmutdinov@gmail.com>
2025-05-10 10:56:09 +0300

    add character highlighting

--- js/view.js
+++ js/view.js
@@ -10,14 +10,32 @@ export class View {
     form: document.querySelector("#form"),
   };
 
+  highLightFilterValue(name, filterValue) {
+    const lowerCaseName = name.toLowerCase();
+    const lowerCaseFilterValue = filterValue.toLowerCase();
+    const startIdx = lowerCaseName.indexOf(lowerCaseFilterValue);
+
+    if (startIdx !== -1) {
+      const start = name.substring(0, startIdx);
+      const interVal = name.substring(startIdx, startIdx + filterValue.length);
+      const end = name.substring(startIdx + filterValue.length);
+      const highLightedName = `${start}<span class="active">${interVal}</span>${end}`;
+      return highLightedName;
+    }
+    return name;
+  }
   renderProducts(arrData) {
     this.elements.productList.innerHTML = "";
     arrData.forEach((product) => {
+      const name = this.highLightFilterValue(
+        product.name,
+        this.elements.filterInut.value
+      );
       if (product.id) {
         const markup = `
             <li>
              <span>${product.subtitle}</span>
-             <h3>${product.name}</h3>
+             <h3>${name}</h3>
              <p>Цена: ${product.price} руб</p>
              <p>Дата добавления: ${product.date}</p>
             </li>
